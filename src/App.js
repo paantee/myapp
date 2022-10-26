c1e600a38b90c3dd09e8c53dd3355b8a8f63b9ea
Teemu Paananen <teemu-paananen@hotmail.com>
2022-10-26 16:31:59 +0300

    datepicker v1

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
 
 import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
 import 'ag-grid-community/styles/ag-theme-material.css'; // Optional theme CSS
+import TextField from '@mui/material/TextField';
+import Button from'@mui/material/Button';
 
 function App() {
 
   const gridRef = useRef();
   const [desc, setDesc] = React.useState({description: '', date: '', priority: ''});
   const [todos, setTodos] = React.useState('');
+  const [date, setDate] = React.useState();
+  
 
   const columns = [  
     { field: "description", sortable: true, filter: true },  
@@ -23,6 +27,7 @@ function App() {
 const inputChanged = (event) => {
   setDesc({...desc, [event.target.name]: event.target.value});
 }
+
 
 const addTodo = (event) => {
   event.preventDefault();
@@ -42,17 +47,18 @@ const deleteTodo = () => {
 <body>
   <h1>Add todo</h1>
     <div className="App">
-      <form onSubmit={addTodo} class="form">
-        <label for="description" name="description">Description</label><br/>
-        <input placeholder="description" name="description" type="text" value={desc.description} onChange={inputChanged}/><br/>
-        <label for="date" name="date">Date</label><br/>
-        <input placeholder="date" name="date" type="text" value={desc.date} onChange={inputChanged}/><br/>
-        <label for="priority" name="priority">Priority</label><br/>
-        <input placeholder="priority" name="priority" type="text" value={desc.priority} onChange={inputChanged}/><br/>
-        <input type="submit" value="Add"/>
-        
-      </form>
       
+      <TextField label="Date"variant="standard"name="date"value={desc.date}onChange={inputChanged}/>
+
+      <TextField label="Description"variant="standard"name="description"value={desc.description}onChange={inputChanged}/>
+
+      <TextField label="Priority"variant="standard"name="priority"value={desc.priority}onChange={inputChanged}/>
+      <Button label="Add" onClick={addTodo}variant="contained" />Add<Button/>
+        
+     
+      
+
+
       <br/>
       
       <button onClick={deleteTodo}>Delete</button>
